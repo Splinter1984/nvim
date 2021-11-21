fe84e79376c79c3939bba58b4157859a53444b3e
Splinter1984 <rom.andrianov1984@gmail.com>
2021-11-21 21:03:43 +0700

    upload new img

--- lua/settings.lua
+++ lua/settings.lua
@@ -5,26 +5,25 @@ cmd 'syntax enable'
 cmd 'filetype plugin indent on'
 cmd 'set guifont=Hack\\ Nerd\\ Font\\ Mono:h9'
 cmd 'language en_US.utf-8'
-
+cmd 'set mouse=a'
 
 vim.g.visualbell = true
-vim.g.mouse = 'a'
 vim.bo.autoread = true
-vim.g.backspace = '2'
+vim.g.backspace = indent
 vim.wo.cursorline = true
 
 vim.g.wildmenu = true
 vim.g.expandtab = true
-vim.g.shiftwidth = '2'
+vim.g.shiftwidth = indent
 vim.g.foldcolumn = '1'
 vim.g.noerrorbells = true
 vim.g.smartcase = true
 vim.g.smartindent = true
-vim.g.tabstop = '2'
+vim.g.tabstop = indent
 vim.o.ignorecase = true
 vim.wo.number = true
 vim.g.shiftround = true
 vim.g.ruler = true
 
 
-cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'
+--cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'
