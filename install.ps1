383c2096b749aa3064c9620eaa2e3523384aa24b
Splinter1984 <rom.andrianov1984@gmail.com>
2022-04-25 04:22:20 +0800

    update ps1 script

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-$NVIM_DIR = "AppData\Local"
+$NVIM_DIR = "$HOME\AppData\Local"
 
 Write-Output "This script will delete all files in $NVIM_DIR\nvim if they exist"
 $Ans = Read-Host "You want to continue? y-[yes] n-[no]"
@@ -7,5 +7,5 @@ if ($Ans -eq "y")
 {
     Remove-Item $NVIM_DIR/nvim -Force
     New-Item -Path $NVIM_DIR -Name "nvim" -ItemType "directory"
-    Copy-Item -Path (Get-Item .).FullName\* -Destination $NVIM_DIR\nvim -recurse -Force
+    Copy-Item -Path "$(Get-Item .)\*" -Destination "$NVIM_DIR/nvim" -recurse -Force
 }
