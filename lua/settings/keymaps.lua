f53bb4980df299ad3413311209761c9c69fb6934
Splinter1984 <rom.andrianov1984@gmail.com>
2022-05-13 06:44:45 +0800

    add buffere keymaps

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -7,12 +7,13 @@ function map(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-map('n', '<C-Right>', ':tabnext<CR>', {noremap = true, silent = true})
-map('n', '<C-Left>', ':tabprevious<CR>', {noremap = true, silent = true})
-map('n', '<C-n>', ':tabnew<CR>', {noremap = true, silent = true})
-map('n', '<C-x>', ':tabclose<CR>', {noremap = true, silent = true})
+--map('n', '<C-Right>', ':tabnext<CR>', {noremap = true, silent = true})
+--map('n', '<C-Left>', ':tabprevious<CR>', {noremap = true, silent = true})
+--map('n', '<C-n>', ':tabnew<CR>', {noremap = true, silent = true})
+--map('n', '<C-x>', ':tabclose<CR>', {noremap = true, silent = true})
 map('n', '<Leader>cd', ':cd %:p:h<CR>:pwd<CR>', {noremap = false, silent = false})
 
 -- buffer keymaps
-map('n', '<C-,>', ':BufferLineCyclePrev<CR>', {noremap = false, silent = true})
-map('n', '<C-.>', ':BufferLineCycleNext<CR>', {noremap = false, silent = true})
+map('n', '<C-Left>', ':BufferLineCyclePrev<CR>', {noremap = true, silent = true})
+map('n', '<C-Right>', ':BufferLineCycleNext<CR>', {noremap = true, silent = true})
+map('n', '<C-x>', ':BufferLinePickClose<CR>', {noremap = true, silent = true})
