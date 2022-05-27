68f40432fb4a043f3fad96fb35fef63106b6356f
Splinter1984 <rom.andrianov1984@gmail.com>
2022-05-27 09:51:40 +0800

    resolve some problems

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -31,7 +31,8 @@ local function on_attach(client, bufnr)
     require('config.lsp.keymaps').setup(client, bufnr)
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()) -- for nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 local opts = {
     on_attach = on_attach,
