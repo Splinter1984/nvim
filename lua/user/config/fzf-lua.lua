3755d399850c3e569246719390d3569c7b9c9f14
Splinter1984 <rom.andrianov1984@gmail.com>
2022-09-09 04:39:06 +0800

    remove pre-settings for languages

---
+++ lua/user/config/fzf-lua.lua
@@ -0,0 +1,7 @@
+local cmp_status_ok, fzf_lua = pcall(require, "fzf_lua")
+if not cmp_status_ok then
+    return
+end
+
+fzf_lua.setup()
+
--- lua/user/config/lsp/init.lua
+++ lua/user/config/lsp/init.lua
@@ -3,6 +3,7 @@ if not status_ok then
     return
 end
 
+
 -- use recommended settigns
 lsp.preset('recommended')
 lsp.setup()
