15e66ffd5cd61f40e64a3a76cf5bec2a7f0454b3
Roman Andrianov <rom.andrianov1984@gmail.com>
2022-12-26 03:27:27 +0800

    remove comment code

--- lua/user/settings/options.lua
+++ lua/user/settings/options.lua
@@ -47,4 +47,4 @@ vim.cmd "set wildmenu"
 vim.cmd "set wildmode=longest:full,full"
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
-vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+-- vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
