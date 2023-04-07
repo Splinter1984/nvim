477d89e228b7dff8bf501b5c87baf2daebb2b9f4
Splinter1984 <rom.andrianov1984@gmail.com>
2023-04-07 19:04:15 +0200

    cleanup

--- init.lua
+++ init.lua
@@ -1,4 +1,20 @@
-require "user.settings"
-require "user.plugins"
-require "user.config"
+--[[      
+          .          .
+        ';;,.        ::'
+      ,:::;,,        :ccc,
+     ,::c::,,,,.     :cccc,
+     ,cccc:;;;;;.    cllll,
+     ,cccc;.;;;;;,   cllll;
+     :cccc; .;;;;;;. coooo;
+     ;llll;   ,:::::'loooo;
+     ;llll:    ':::::loooo:
+     :oooo:     .::::llodd:
+     .;ooo:       ;cclooo:.
+       .;oc        'coo;.
+         .'         .,.   
+      author: @splinter1984 
+]]--
+
+require "core"
+require "plugins"
 
